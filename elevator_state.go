e2ab958136fcc5b65f66a2b385d8976f387b6e98
iverleirvik <iver.leirvik@gmail.com>
2025-02-17 10:44:17 +0100

    Continued with trying to make a fsm.

--- elevator_state.go
+++ elevator_state.go
@@ -19,23 +19,47 @@ const (
 	EB_Moving                    = 2
 )
 
+// Created a button type for use in statemachine to decide further action.
+type Button int
+
+const (
+	D_Down Button = -1
+	D_Stop        = 0
+	D_Up          = 1
+)
+
 // Create elevator object
+var Elevator elevator
 
 // Create state machine functions:
 // Needs to react on button press and floor
 
 func elevatorInit() {
+	// TODO: Make sure this is really necessary.
+	elevio.SetMotorDirection(elevio.MD_Down)
+	Elevator.dirn = elevio.MD_Down
+	Elevator.behavior = EB_Moving
 
 }
 
-func buttonPress() {
+func buttonPress(btnFloor int, btnType Button) {
+	switch Elevator.behavior {
+	case EB_DoorOpen:
 
+	case EB_Moving:
+
+	case EB_idle:
+	}
 }
 
 func floorArrival() {
-
+	switch Elevator.behavior {
+	case EB_Moving:
+	}
 }
 
 func doorTimeout() {
-
+	switch Elevator.behavior {
+	case EB_DoorOpen:
+	}
 }
