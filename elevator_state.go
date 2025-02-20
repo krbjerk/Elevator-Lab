7dac204cf940db550fc70ec9c1e1388125063f9c
iverleirvik <iver.leirvik@gmail.com>
2025-02-20 14:49:30 +0100

    Worked further on exercise 3. Specifically finite state machine. Aqquired a better understanding of request handling. Started making some functions for that and fsm.

--- elevator_state.go
+++ elevator_state.go
@@ -7,7 +7,7 @@ import (
 type elevator struct {
 	floor    int
 	dirn     elevio.MotorDirection
-	request  [4][3]int
+	requests [4][3]int
 	behavior ElevatorBehavior
 }
 
@@ -43,23 +43,52 @@ func elevatorInit() {
 }
 
 func buttonPress(btnFloor int, btnType Button) {
+	// Requests can happen at any point.
+
 	switch Elevator.behavior {
 	case EB_DoorOpen:
+		// Clear if request is for same floor
+		// Otherwise request will be approved and stored for later
+		if Elevator.floor == btnFloor {
+			Elevator.floor = btnFloor
+			// Start timer for door open
+		} else {
+			Elevator.requests[btnFloor][btnType] = 1
+		}
 
 	case EB_Moving:
-
+		// Take request and store it for later
+		Elevator.requests[btnFloor][btnType] = 1
 	case EB_idle:
+		// Take request and act on it immediately
+		Elevator.requests[btnFloor][btnType] = 1
+		// TODO
+
+		// Based on the requests create a "twin" that knows what the elevator should do
+		// We will use that twin to actually make the elevator do
+
 	}
 }
 
 func floorArrival() {
-	switch Elevator.behavior {
-	case EB_Moving:
-	}
+	// Moving given implicitly
+
+	// Check if the current request needs to stop at this floor
+	//	Stop the cab
+	//	Do something with the lights
+	//	Clear request
+	//	Open door and start timer
+
 }
 
 func doorTimeout() {
-	switch Elevator.behavior {
-	case EB_DoorOpen:
-	}
+	// Idle given implicitly
+
+	// check if there are requests
+	// Act on them
+
 }
+
+// TODO: We need some request prioritization
+
+// We need some system to handle requests
---
+++ requests.go
@@ -0,0 +1,34 @@
+package main
+
+//
+
+import (
+	"Driver-go/elevio"
+)
+
+type twin struct {
+	dirn     elevio.MotorDirection
+	behavior ElevatorBehavior
+}
+
+// Functions for simplification
+
+func requestAbove() {
+
+}
+
+func requestBelow() {
+
+}
+
+func requestHere() {
+
+}
+
+// request choose direction
+
+// request should stop
+
+// request should clear immediately
+
+// request clear at current floor
