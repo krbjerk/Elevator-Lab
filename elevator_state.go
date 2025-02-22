86627dbb49c58549080a61296ea3e70443bc8f2e
iverleirvik <iver.leirvik@gmail.com>
2025-02-22 21:25:11 +0100

    Continued on with asgmnt 3. Finished request functions and soon to be finished with fsm.

--- elevator_state.go
+++ elevator_state.go
@@ -14,18 +14,26 @@ type elevator struct {
 type ElevatorBehavior int
 
 const (
-	EB_idle     ElevatorBehavior = 0
+	EB_Idle     ElevatorBehavior = 0
 	EB_DoorOpen                  = 1
 	EB_Moving                    = 2
 )
 
 // Created a button type for use in statemachine to decide further action.
+type Direction int
+
+const (
+	D_Down Direction = -1
+	D_Stop           = 0
+	D_Up             = 1
+)
+
 type Button int
 
 const (
-	D_Down Button = -1
-	D_Stop        = 0
-	D_Up          = 1
+	B_HallUp   Button = 0
+	B_HallDown        = 1
+	B_Cab             = 2
 )
 
 // Create elevator object
@@ -50,7 +58,6 @@ func buttonPress(btnFloor int, btnType Button) {
 		// Clear if request is for same floor
 		// Otherwise request will be approved and stored for later
 		if Elevator.floor == btnFloor {
-			Elevator.floor = btnFloor
 			// Start timer for door open
 		} else {
 			Elevator.requests[btnFloor][btnType] = 1
@@ -59,18 +66,34 @@ func buttonPress(btnFloor int, btnType Button) {
 	case EB_Moving:
 		// Take request and store it for later
 		Elevator.requests[btnFloor][btnType] = 1
-	case EB_idle:
+	case EB_Idle:
 		// Take request and act on it immediately
 		Elevator.requests[btnFloor][btnType] = 1
 		// TODO
+		twin := Elevator.requestDirection()
+		Elevator.dirn = twin.dirn
+		Elevator.behavior = twin.behavior
 
-		// Based on the requests create a "twin" that knows what the elevator should do
-		// We will use that twin to actually make the elevator do
+		switch twin.behavior {
+		case EB_DoorOpen:
+			// Do something about the doorlight
+			// Start the door timer
+			Elevator = Elevator.requestsClearAtCurrentFloor()
+
+		case EB_Moving:
+			//
+			elevio.SetMotorDirection(Elevator.dirn)
+
+		case EB_Idle:
+		}
+
+		// SetAlllights(Elevator)
+		// Print out something about the elevator having a new state.
 
 	}
 }
 
-func floorArrival() {
+func floorArrival(newFloor int) {
 	// Moving given implicitly
 
 	// Check if the current request needs to stop at this floor
@@ -79,6 +102,24 @@ func floorArrival() {
 	//	Clear request
 	//	Open door and start timer
 
+	// Print out that the elevator has arrived at a new floor
+	Elevator.floor = newFloor
+	elevio.SetFloorIndicator(Elevator.floor)
+
+	switch Elevator.behavior {
+	case EB_Moving:
+		if Elevator.requestsShouldStop() {
+			elevio.SetMotorDirection(elevio.MD_Stop)
+			elevio.SetDoorOpenLamp(true)
+			Elevator = Elevator.requestsClearAtCurrentFloor()
+			// Start open door timer
+			// Set all lights
+			Elevator.behavior = EB_DoorOpen
+		}
+	default:
+
+	}
+	// Print the new state of elevator.
 }
 
 func doorTimeout() {
@@ -87,6 +128,29 @@ func doorTimeout() {
 	// check if there are requests
 	// Act on them
 
+	// Elevator printout
+	switch Elevator.behavior {
+	case EB_DoorOpen:
+		twin := Elevator.requestDirection()
+		Elevator.dirn = twin.dirn
+		Elevator.behavior = twin.behavior
+
+		switch Elevator.behavior {
+		case EB_DoorOpen:
+			// Start timer open door
+			Elevator = Elevator.requestsClearAtCurrentFloor()
+			// Set all lights
+		case EB_Moving:
+
+		case EB_Idle:
+			elevio.SetDoorOpenLamp(false)
+			elevio.SetMotorDirection(Elevator.dirn)
+		}
+
+	default:
+
+	}
+	// Print elevator state
 }
 
 // TODO: We need some request prioritization
--- requests.go
+++ requests.go
@@ -6,29 +6,116 @@ import (
 	"Driver-go/elevio"
 )
 
-type twin struct {
+type Twin struct {
 	dirn     elevio.MotorDirection
 	behavior ElevatorBehavior
 }
 
 // Functions for simplification
 
-func requestAbove() {
+func (e elevator) requestAbove() bool {
+	for i := e.floor + 1; i < 4; i++ {
+		for j := 0; j < 3; j++ {
+			if e.requests[i][j] == 1 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (e elevator) requestBelow() bool {
+	for i := 0; i < e.floor; i++ {
+		for j := 0; j < 3; j++ {
+			if e.requests[i][j] == 1 {
+				return true
+			}
+		}
+	}
+	return false
 
 }
 
-func requestBelow() {
-
-}
-
-func requestHere() {
-
+func (e elevator) requestHere() bool {
+	for i := 0; i < 3; i++ {
+		if e.requests[e.floor][i] == 1 {
+			return true
+		}
+	}
+	return false
 }
 
 // request choose direction
 
+func (e elevator) requestDirection() Twin {
+	switch e.dirn {
+	case elevio.MD_Up:
+		if e.requestAbove() {
+			return Twin{elevio.MD_Up, EB_Moving}
+		} else if e.requestHere() {
+			return Twin{elevio.MD_Down, EB_DoorOpen}
+		} else if e.requestBelow() {
+			return Twin{elevio.MD_Down, EB_Moving}
+		} else {
+			return Twin{elevio.MD_Stop, EB_Idle}
+		}
+
+	case elevio.MD_Down:
+		if e.requestBelow() {
+			return Twin{elevio.MD_Down, EB_Moving}
+		} else if e.requestHere() {
+			return Twin{elevio.MD_Up, EB_DoorOpen}
+		} else if e.requestAbove() {
+			return Twin{elevio.MD_Up, EB_Moving}
+		} else {
+			return Twin{elevio.MD_Stop, EB_Idle}
+		}
+
+	case elevio.MD_Stop:
+		if e.requestHere() {
+			return Twin{elevio.MD_Stop, EB_DoorOpen}
+		} else if e.requestAbove() {
+			return Twin{elevio.MD_Up, EB_Moving}
+		} else if e.requestBelow() {
+			return Twin{elevio.MD_Down, EB_Moving}
+		} else {
+			return Twin{elevio.MD_Stop, EB_Idle}
+		}
+	default:
+		return Twin{elevio.MD_Stop, EB_Idle} // Must include default to not get no return error.
+	}
+}
+
 // request should stop
+func (e elevator) requestsShouldStop() bool {
+	switch e.dirn {
+	case elevio.MD_Down:
+		return (e.requests[e.floor][B_HallDown] == 1) ||
+			(e.requests[e.floor][B_Cab] == 1) ||
+			!e.requestBelow()
+
+	case elevio.MD_Up:
+		return (e.requests[e.floor][B_HallUp] == 1) ||
+			(e.requests[e.floor][B_Cab] == 1) ||
+			!e.requestAbove()
+
+	case elevio.MD_Stop:
+		fallthrough
+	default:
+		return true
+	}
+}
 
 // request should clear immediately
 
+// Already implemented in the function buttonPress().
+
 // request clear at current floor
+// Function implemented for CV_All
+func (e elevator) requestsClearAtCurrentFloor() elevator {
+	for btn := 0; btn < 3; btn++ {
+		e.requests[e.floor][btn] = 0
+	}
+
+	return e
+}
